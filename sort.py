e8b56863d8acf3275b58c179359d3c941dff26b0
Andrey <vpbotty@gmail.com>
2019-09-13 17:43:43 +0700

    add selection sort

--- sort.py
+++ sort.py
@@ -9,7 +9,19 @@ def bubble_sort(array):
     return array
 
 
-b = aw.fill_array_random(20)
-b = bubble_sort(b)
+def selection_sort(array):
+    for i in range(len(array)):
+        min = i
+        j = i + 1
+        while j < len(array):
+            if array[j] < array[min]:
+                min = j
+            j += 1
+        array[i], array[min] = array[min], array[i]
+    return array
+
+
+b = aw.fill_array_random(7)
+b = selection_sort(b)
 for i in range(len(b)):
     print(b[i])
