2e42d3a5c54d1be642bf0f401393821d3fc03d07
Andrey <vpbotty@gmail.com>
2019-09-13 17:22:51 +0700

    pretify all

--- array_worker.py
+++ array_worker.py
@@ -1,15 +1,19 @@
 import random
+
+
 def fill_array_standart(array_len):
     array = []
     for i in range(array_len):
         array.append(i)
     return array
 
+
 def fill_array_reverse(array_len):
     array = []
     for i in range(array_len):
         array.append(array_len - i)
     return array
+
 
 def fill_array_random(array_len):
     array = []
--- sort.py
+++ sort.py
@@ -1,12 +1,15 @@
 import array_worker as aw
 
-def buble_sort(array):
-    for i in range(len(array)-1, 0, -1):
+
+def bubble_sort(array):
+    for i in range(len(array) - 1, 0, -1):
         for j in range(i):
-            if array[j] > array[j+1]:
-                array[j], array[j+1] = array[j+1], array[j]
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
     return array
+
+
 b = aw.fill_array_random(20)
-b = buble_sort(b)
+b = bubble_sort(b)
 for i in range(len(b)):
     print(b[i])
